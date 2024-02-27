5d60ca5b6c582a66d7f522afc9102e54e58b507c
Crowdbotics <team@crowdbotics.com>
2024-02-27 10:58:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-wind-46661",
+  url: "https://divine-wind-46661.botics.co",
+  api: "https://divine-wind-46661.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
